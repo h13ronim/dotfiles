edcf8cf58a7c09d0cf93bc8b55ef6177fe1e4921
Marcin Michalowski <marcin@stick.pl>
2016-09-01 10:42:29 +0200

    Add nproc command to macos

--- plugins/osx-plus/osx-plus.plugin.zsh
+++ plugins/osx-plus/osx-plus.plugin.zsh
@@ -5,3 +5,5 @@ function bluetooth-not-available-fix() {
   echo "If bluetooth doesn't work after restart then please reset the SMC."
   echo "How to reset the SMC: https://support.apple.com/en-us/HT201295"
 }
+
+alias nproc='sysctl -n hw.ncpu'
