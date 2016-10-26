44e1a9df768e7e0f55b40514200ba24c05c02eae
Marcin Michalowski <marcin@stick.pl>
2016-10-26 20:51:00 +0200

    Fix git install

--- scripts/git.sh
+++ scripts/git.sh
@@ -4,6 +4,6 @@ install_git() {
   which git > /dev/null
   if [ $? -gt 0 ]; then
     echo "Installing git..."
-    sudo apt-get install git || exit $?
+    sudo apt-get install git || return $?
   fi
 }
--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-source scripts/git.sh
-install_git || exit $?
+which git > /dev/null
+if [ $? -gt 0 ]; then
+  echo "Installing git..."
+  sudo apt-get install git || exit $?
+fi
 
 mkdir -p $HOME/projects || exit $?
 
 git clone git@gitlab.com:h13ronim/dotfiles.git $HOME/projects/dotfiles
 
-cd $HOME/projects/dotfiles
+echo "go to ~/projects/dotfiles and run ./install.sh"
