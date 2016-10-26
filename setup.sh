866abf7bd2a551a93233cdcb4bc9fd67af3c76c7
Marcin Michalowski <marcin@stick.pl>
2016-10-26 10:38:56 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+which git > /dev/null
+if [ $? -gt 0 ]; then
+  echo "Installing git..."
+  sudo apt-get install git || exit $?
+fi
+
+mkdir -p $HOME/projects || exit $?
+
+git clone git@gitlab.com:h13ronim/dotfiles.git $HOME/projects/dotfiles
+
+cd $HOME/projects/dotfiles
