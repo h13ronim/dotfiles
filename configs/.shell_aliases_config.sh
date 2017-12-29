0c9a1cfd75d9b6041a285de4b990391e8d140901
Marcin Michalowski <marcin@stick.pl>
2017-12-29 22:00:10 +0100

    Add shell_aliases_compiler config.sh

---
+++ configs/.shell_aliases_config.sh
@@ -0,0 +1,13 @@
+export SOURCES="
+  git@github.com:pr0d1r2/plexus.git:bash_profile.d
+  git@github.com:pr0d1r2/ruby_dev_shell_aliases.git:.
+  git@github.com:pr0d1r2/git_shell_aliases.git:.
+  git@github.com:pr0d1r2/github_shell_aliases.git:.
+  git@github.com:pr0d1r2/osx_shell_aliases.git:.
+  git@github.com:h13ronim/atom_shell_aliases.git:.
+  git@github.com:pr0d1r2/common_shell_aliases.git:.
+  git@gitlab.com:doubledrones/dt_shell_aliases.git:.
+  git@gitlab.com:doubledrones/doubledrones_shell_aliases.git:.
+  $HOME/projects/local_shell_aliases
+  $HOME/projects/shell_aliases_compiler/shell_aliases.d
+"
