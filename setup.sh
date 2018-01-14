1d7685505d7e1ec54661fd2493ce8a52602ae4b6
Marcin Michalowski <marcin@stick.pl>
2018-01-14 21:10:58 +0100

    Add youtube-dl config

--- setup.sh
+++ setup.sh
@@ -2,13 +2,23 @@
 
 D_R=`cd \`dirname $0\` ; pwd -P`
 
-for FILE in \
-  gemrc \
-  gitconfig \
-  pryrc \
-  rubocop.yml \
-  vimrc.after \
+setup_basic_configs() {
+  for FILE in \
+    gemrc \
+    gitconfig \
+    pryrc \
+    rubocop.yml \
+    vimrc.after \
 
-do
-  ln -sf $D_R/configs/$FILE $HOME/.$FILE || exit $?
-done
+  do
+    ln -sf $D_R/configs/$FILE $HOME/.$FILE || return $?
+  done
+}
+
+setup_youtube_dl_config() {
+  mkdir -p $HOME/.config/youtube-dl/ || return $?
+  ln -sf $D_R/configs/youtube-dl-config $HOME/.config/youtube-dl/config
+}
+
+setup_basic_configs
+setup_youtube_dl_config
