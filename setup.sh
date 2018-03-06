fd85fae4a180268ea0beb4e48d36863d12bc3707
Marcin Michalowski <marcin@stick.pl>
2018-03-06 21:15:01 +0100

    Assign default applications

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ main() {
   setup_basic_configs
   setup_youtube_dl_config
   setup_mpv_config
+  assign_default_applications
 }
 
 setup_basic_configs() {
@@ -31,6 +32,15 @@ setup_mpv_config() {
     rm -rf $HOME/.config/mpv || return $?
   fi
   ln -sf $D_R/configs/mpv $HOME/.config/mpv
+}
+
+assign_default_applications() {
+  if [ ! -f /usr/local/bin/duti ]; then
+    brew install duti
+  fi
+  for EXT in {avi,mkv,mp4}; do
+    duti -s io.mpv "${EXT}" viewer
+  done
 }
 
 main
