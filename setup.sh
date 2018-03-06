4c023016b92cd79edcf9d062ff2453bf33e7f103
Marcin Michalowski <marcin@stick.pl>
2018-03-06 21:11:12 +0100

    Add mpv

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ D_R=`cd \`dirname $0\` ; pwd -P`
 main() {
   setup_basic_configs
   setup_youtube_dl_config
+  setup_mpv_config
 }
 
 setup_basic_configs() {
@@ -23,6 +24,13 @@ setup_basic_configs() {
 setup_youtube_dl_config() {
   mkdir -p $HOME/.config/youtube-dl/ || return $?
   ln -sf $D_R/configs/youtube-dl-config $HOME/.config/youtube-dl/config
+}
+
+setup_mpv_config() {
+  if [ -d $HOME/.config/mpv ]; then
+    rm -rf $HOME/.config/mpv || return $?
+  fi
+  ln -sf $D_R/configs/mpv $HOME/.config/mpv
 }
 
 main
